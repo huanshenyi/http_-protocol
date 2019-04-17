23d6661a54b1b4f9eec3acd6bff195a10502c7b1
huanshenyi <txy1226052@gmail.com>
2019-04-17 23:45:25 +0900

    Cache-Contro

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const fs = require('fs')
 http.createServer(function (request, response){
     console.log('request come',request.url)
     
+  if(request.url === '/'){
     const http = fs.readFileSync('test.html','utf8')
     response.writeHead(200,{
         'Content-Type': 'text/html'
     })
     response.end(http)
+  }
+//よくある方法はbuild後jsのコートをhash化し、routeとして保存,そうすれば内容変換するまでにcacheは保存される
+  if(request.url === '/script.js'){
+    response.writeHead(200,{
+        'Content-Type': 'text/javascript',
+        'Cache-Control': 'max-age=200,public'
+    })
+    response.end('console.log("script loaded twice")')
+  }
 }).listen(8888)
 
 console.log('server listening on 8888')
